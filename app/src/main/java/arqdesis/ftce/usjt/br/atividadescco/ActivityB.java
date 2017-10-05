9dff4da12cf8ddd0a0c61ee237d18902849cf6d2
arqdsis <arqdsis@mocli10217.animaedu.intranet>
2017-10-05 20:50:15 -0300

    Initial commit

---
+++ app/src/main/java/arqdesis/ftce/usjt/br/atividadescco/ActivityB.java
@@ -0,0 +1,13 @@
+package arqdesis.ftce.usjt.br.atividadescco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
---
+++ app/src/main/java/arqdesis/ftce/usjt/br/atividadescco/ActivityC.java
@@ -0,0 +1,13 @@
+package arqdesis.ftce.usjt.br.atividadescco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityC extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
